f6b7a6dad78ab8c5b437e96a625e7dc86c8844c5
yuvrajnautiyal-max <yuvraj.nautiyal611@gmail.com>
2026-09-03 08:06:07 +0530

    Add files via upload

---
+++ yuvraj 100 days codes/day25_2.c
@@ -0,0 +1,32 @@
+// Q50 (Nested Loops without Arrays/Strings)
+// Write a program to print the following pattern:
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= rows - i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
